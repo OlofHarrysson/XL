d8f4ab167f4d5d3e8454db4ad17c560b4a296c15
DjungelJarl <shan.langlais@gmail.com>
2014-12-05 22:14:47 +0100

    implemented make xcellexpr in xcellfactory(almost)

--- src/model/XCellFactory.java
+++ src/model/XCellFactory.java
@@ -1,7 +1,9 @@
 package model;
 
+import java.io.IOException;
+
 public interface XCellFactory {
 	
 	public XCell makeXCellComment(String cellRef, String content);
-	public XCell makeXCellExpr(String cellRef, String content);
+	public XCell makeXCellExpr(String cellRef, String content) throws IOException;
 }
--- src/model/XCellFactoryImplementation.java
+++ src/model/XCellFactoryImplementation.java
@@ -1,14 +1,28 @@
 package model;
 
+import java.io.IOException;
+
+import expr.Expr;
+import expr.ExprParser;
+
 public class XCellFactoryImplementation implements XCellFactory {
 	
 	public XCell makeXCellComment(String cellRef, String content) {
-		return new XCellComment("heasdasd");
+		return new XCellComment(content);
 	}
 
 	
-	public XCell makeXCellExpr(String cellRef, String content) {
-		return null;
+	public XCell makeXCellExpr(String cellRef, String content) throws IOException {
+		ExprParser parser;
+		Expr expr;
+		try{
+			parser = new ExprParser();
+			expr = parser.build(content);
+		} catch (IOException e) {
+			throw new IOException(e.getMessage());
+		}
+		
+		return new XCellExpr(expr);
 	}
 
 }
