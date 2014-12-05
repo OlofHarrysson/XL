727cde71e9c8f8fca41b54aa06119be3e94379c0
DjungelJarl <shan.langlais@gmail.com>
2014-12-05 21:55:47 +0100

    test, mul developers

--- src/model/XCellFactoryImplementation.java
+++ src/model/XCellFactoryImplementation.java
@@ -1,9 +1,14 @@
 package model;
 
-public class XCellFactoryImplementation {
+public class XCellFactoryImplementation implements XCellFactory {
 	
 	public XCell makeXCellComment(String cellRef, String content) {
 		return new XCellComment("heasdasd");
+	}
+
+	
+	public XCell makeXCellExpr(String cellRef, String content) {
+		return null;
 	}
 
 }
